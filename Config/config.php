13ee6218e94f7d30552b3d6998ab7f810294d691
IAssine33 <yassine.zriba@lapiscine.pro>
2024-07-15 15:19:14 +0200

    Creation du login pour mySQL sur la BDD

--- Config/config.php
+++ Config/config.php
@@ -5,4 +5,19 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_set_cookie_params(3600);
+
+
+// Creation des variables pour se connecter sur mySQL et se loguer 
+$dsn = 'mysql:host=localhost:3306;dbname=piscine_php';  //(chemin d'accés pour le serveur sur la base de donner)
+$username = 'root';
+$password = 'root';
+
+//  Essayer de se connecter via les variables créer("chemin d'accés pour le serveur sur la base de donner", username, password)
+try {
+    $pdo = new PDO($dsn, $username, $password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+
 ?>
--- meal.php
+++ meal.php
@@ -1,6 +1,6 @@
 
 <?php
-class meal {
+abstract class meal {
 
     protected $size;
 
