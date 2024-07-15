1863ea30f732b9cfa2d49920172de15310bda76d
IAssine33 <yassine.zriba@lapiscine.pro>
2024-07-15 16:52:25 +0200

    Mettre une valeur dans la colonne description

--- controller/productsBddController.php
+++ controller/productsBddController.php
@@ -6,16 +6,18 @@ require_once('../config/config.php');
 
 // Inserer dans la table "products" des valeurs dans les colonnes 'titre' et 'sous_titre'.
 
-$sql = "INSERT INTO products (titre, sous_titre) VALUES (:titre, :sous_titre)";
+$sql = "INSERT INTO products (titre, sous_titre, description) VALUES (:titre, :sous_titre, :description)";
 $stmt = $pdo->prepare($sql);
 
 // Créer les variables avec la valeur qu'on veut mettre. 
 $titre = "Exemple valeur de titre";
 $sous_titre = "Exemple valeur de sous-titre";
+$description = "Ceci est une description";
 
 // ???Nettoyage des valeur mise dans les variables.
-$stmt->bindParam(':titre', $titre);
+$stmt->bindParam(':titre', $titre); // pour dire verifier s'il n'y a pas de caractére (bizard ou SQL,...) dans :titre avant de le mettre dans la variable $titre.
 $stmt->bindParam(':sous_titre', $sous_titre);
+$stmt->bindParam(':description', $description);
 
 // ???Condition d'execution, de verification d'erreur.
 if ($stmt->execute()) {
